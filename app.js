650e2fe515ef729837dc44ece598d3c1aca6e3db
lore198429 <lore198429@gmail.com>
2021-06-10 19:58:21 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//Array que contiene los datos para mostrar
+let usersList = [
+  {
+    name: "Daniel",
+    lastName: "Meza Miranda",
+    email: "daniel.academlo@gmail.com",
+  },
+  {
+    name: "Juan",
+    lastName: "Martínez",
+    email: "correo@correo.com",
+  },
+  {
+    name: "Samantha",
+    lastName: "López",
+    email: "samantha@correo.com",
+  },
+];
